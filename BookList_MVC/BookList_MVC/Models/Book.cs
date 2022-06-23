718b370421c75185962f53b9380997935c5e56b6
Tiago Costa <tiagodpv@gmail.com>
2022-06-22 22:21:11 -0400

    connection to Db

---
+++ BookList_MVC/BookList_MVC/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_MVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }       
+        
+        
+    }
+}
